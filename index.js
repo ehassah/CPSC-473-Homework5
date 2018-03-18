139dd5d39ba07c16699cd7f6faec54a4ca0aa8ad
ehassah <ehassah@gmail.com>
2018-03-17 23:06:03 -0700

    Created WebSocket connection

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 var http = require("http");
 var fs = require("fs");
 var extract = require("./extract");
+var wss = require("./websockets-server");
 
 var handleError = function (err, res){
-  res.writeHead(404);
-  res.end();
+  // res.writeHead(404);
+  res.setHeader("Content-Type", "text/html");
+  fs.readFile("app/error.html", function(err, data){
+  res.end(data);
+  });
 };
 
 var server = http.createServer(function (req, res){
   console.log("Responding to a request.");
-
   var filePath = extract(req.url);
   fs.readFile(filePath, function(err, data){
     if(err){
       handleError(err, res);
-      return;
+    return;
     }else{
+      res.setHeader("Content-Type", "text/html");
       res.end(data);
     }
   });
