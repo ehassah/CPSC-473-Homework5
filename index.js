8762cb2c7bb6ad2a21e526d338e2b2d10b91ff16
ehassah <ehassah@gmail.com>
2018-03-19 19:34:54 -0700

    Fixed dynamic MIME type

--- index.js
+++ index.js
@@ -2,20 +2,12 @@ var http = require("http");
 var fs = require("fs");
 var extract = require("./extract");
 var wss = require("./websockets-server");
-// var mime = require("mime");
-var mime = require('mime-types')
+var mime = require("mime");
 
 var handleError = function (err, res){
-  // res.writeHead(404);
-  // res.setHeader("Content-Type", "text/html");
-// var mimeType = contentType.getValue().split(";")[0].trim();
-var fileName = "error.html";
-var type  = mime.contentType(fileName);
-// console.log("mime type is "+ type);
-  res.setHeader("Content-Type", type);
-  fs.readFile(fileName, function(err, data){
+  res.setHeader("Content-Type", "text/html");
+  fs.readFile("app/error.html", function(err, data){
     res.end(data);
-    // window.location = fileName;
   });
 };
 
@@ -25,9 +17,11 @@ var server = http.createServer(function (req, res){
   fs.readFile(filePath, function(err, data){
     if(err){
       handleError(err, res);
-    return;
+      return;
     }else{
-      res.setHeader("Content-Type", "text/html");
+      var mimeType = mime.getType(filePath);
+      console.log(mimeType);
+      res.setHeader("Content-Type", mimeType);
       res.end(data);
     }
   });
